9e4d884c792b7f5382b0ef18576660d47628faab
Ahmed Aly El-Ghannam <ahmedaly.g112@gmail.com>
2023-08-15 15:14:29 +0200

    Day 10 Done + smol Terminal Module Update
    Day 10:
    1. Switches.
    2. Layered Architecture.
    3. DIO Drivers
    Bonus:
    1. Finished one function in Terminal Module.
    2. Created a folder to collect drivers.

--- Graduation_Project/C_Project/Payment_App/Code/Terminal/terminal.c
+++ Graduation_Project/C_Project/Payment_App/Code/Terminal/terminal.c
@@ -2,3 +2,31 @@
 #include <stdlib.h>
 #include "terminal.h"
 
+u8 isValidDate(u8 *str)
+{
+	
+}
+
+
+EN_terminalError_t getTransactionDate(ST_terminalData_t *termData)
+{
+	u8 temp[10]; // DD/MM/YYYY
+	fgets(temp, 10, stdin);
+	fflush(stdin);
+	u8 size;
+	for (size = 0; temp[size] != '\0'; size++);
+	if (size == 10)
+	{
+		if (isValidDate(&temp[0]))
+		{
+			u8 i;
+			u8 *tran_date = termData->transactionDate;
+			for (i = 0; temp[i] != '\0'; i++)
+				tran_date[i] = temp[i];
+			return TERMINAL_OK
+		}
+	}
+	
+	return WRONG_DATE;
+}
+
---
+++ Me_Defined_C_Libs_Drivers/MCAL/DIO/main.c
@@ -0,0 +1,8 @@
+#include "../../LIB/STD_Types.h"
+#include "DIO_CFG.h"
+#include "DIO_Interface.h"
+
+void main(void)
+{
+
+}
