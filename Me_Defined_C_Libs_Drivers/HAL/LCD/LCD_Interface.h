477cd14db96b7ea85375c7ef4598933005eda78c
Ahmed Aly El-Ghannam <ahmedaly.g112@gmail.com>
2023-08-17 03:19:02 +0300

    LCD Driver Update + Board Notes for Day 11
    1. LCD_CFG.h is almost done. I may need to edit stuff once I implement the functions.
    2. LCD_Private.h is almost done. Over 140 lines and I still need to test how to write special characters. So far I am happy with how it is turning out.
    3. LCD_interface has all the necessary prototypes minus their arguments description. I will add them one I begin writing the functions themselves.

--- Me_Defined_C_Libs_Drivers/HAL/LCD/LCD_Interface.h
+++ Me_Defined_C_Libs_Drivers/HAL/LCD/LCD_Interface.h
@@ -1,5 +1,13 @@
 #ifndef LCD_INTERFACE_H_
 #define LCD_INTERFACE_H_
 
+void LCD_voidInitDisplay(void);
+void LCD_voidSendCMD(u8 copy_u8Command);
+void LCD_voidDisplayChar(u8 copy_u8Char);
+void LCD_voidDisplayStr(u8 *copy_u8Str);
+void LCD_voidSetCursorPosition(u8 copy_u8XPosition, u8 copy_u8YPosition);
+void LCD_voidDisplayInt(u32 copy_u32Number);
+void LCD_voidSendSpecChar(u8 copy_u8CharIndex, u8 *copy_u8SpecialChar);
+void LCD_voidDisplaySpecChar(u8 copy_u8CharIndex);
 
 #endif
