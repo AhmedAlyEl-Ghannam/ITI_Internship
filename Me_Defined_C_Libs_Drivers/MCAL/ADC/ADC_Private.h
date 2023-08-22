014839f5d68a4e7c0ac2f6996deb58a8393c993a
Ahmed Aly El-Ghannam <ahmedaly.g112@gmail.com>
2023-08-23 01:30:11 +0300

    Day 15 is Done, sorta
    ADC
    Sensors
    Finished studying ADC late so I can't work on its drivers.
    Copefully, I can wake up a little early to finish it.
    Assignments seem easy but I need the driver to be finished.
    TO DO: put cli && sei in a separate MCAL module and name it GI (Global Interrupt) to use them whenever I want without involving EXTI.

---
+++ Me_Defined_C_Libs_Drivers/MCAL/ADC/ADC_Private.h
@@ -0,0 +1,6 @@
+#ifndef ADC_PRIVATE_H_
+#define ADC_PRIVATE_H_
+
+
+
+#endif
---
+++ Me_Defined_C_Libs_Drivers/MCAL/ADC/ADC_Register.h
@@ -0,0 +1,10 @@
+#ifndef ADC_REGISTER_H_
+#define ADC_REGISTER_H_
+
+#define ADMUX (*(volatile u8*)0x27)
+#define ADCSRA (*(volatile u8*)0x26)
+#define ADCL (*(volatile u8*)0x24)
+#define ADCH (*(volatile u8*)0x25)
+#define SFIOR (*(volatile u8*)0x50)
+
+#endif
